18f311ab0d0a4332189b9b8e371ed3a5744526f8
Vanilla <sttsaat@STTs-MacBook-Pro.local>
2025-08-20 09:12:50 +0700

    add: api_service

---
+++ lib/services/api_service.dart
@@ -0,0 +1,80 @@
+// lib/services/api_service.dart
+
+import 'dart:async' show TimeoutException;
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:validator/screens/login_screen.dart';
+
+class ApiService {
+  static const String baseurl =
+      'http://172.172.52.4:8080/pengadaan-new/mobile-validator/';
+  // static const String baseurl = 'https://netunim.seabs.ac.id/api-syc2025/';
+
+  static Future<Map<String, dynamic>> loginUser(
+    String username,
+    String password,
+  ) async {
+    try {
+      final url = Uri.parse('${baseurl}check-user');
+      final response = await http
+          .post(
+            url,
+            headers: {'Content-Type': 'application/json'},
+            body: json.encode({'username': username, 'password': password}),
+          )
+          .timeout(const Duration(seconds: 10));
+
+      if (response.statusCode == 200) {
+        return json.decode(response.body);
+      } else {
+        final error = json.decode(response.body);
+        throw Exception(error['message'] ?? 'Login failed');
+      }
+    } on http.ClientException catch (e) {
+      throw Exception('Network error: $e');
+    } on TimeoutException {
+      throw Exception('Request timed out');
+    } catch (e) {
+      throw Exception('Unexpected error: $e');
+    }
+  }
+
+  // static Future<bool> validateToken(
+  //   BuildContext context, {
+  //   required String token,
+  // }) async {
+  //   if (token == null || token.isEmpty) {
+  //     Navigator.of(context).pushReplacement(
+  //       MaterialPageRoute(builder: (context) => const LoginScreen()),
+  //     );
+  //   }
+
+  //   final url = Uri.parse('${baseurl}brm-today');
+  //   final response = await http.get(
+  //     url,
+  //     headers: {
+  //       'Authorization': 'Bearer $token',
+  //       'Content-Type': 'application/json',
+  //     },
+  //   );
+
+  //   if (response.statusCode == 200) {
+  //     final Map<String, dynamic> dataBacaan = json.decode(response.body);
+  //     return dataBacaan['success'];
+  //   } else if (response.statusCode == 401) {
+  //     showCustomSnackBar(
+  //       context,
+  //       'Sesi login Anda telah habis. Silakan login kembali.',
+  //     );
+  //     await handleUnauthorized(context);
+  //     throw Exception('Unauthorized');
+  //     // return false;
+  //   } else {
+  //     print('❌ Error test: ${response.statusCode} - ${response.body}');
+  //     throw Exception('Failed to validate token');
+  //     // return false;
+  //   }
+  // }
+}
